f56b8a4f3da4ca3461e09bdf4c49bd475d23d74d
Stef Claus <stef.claus@gmail.com>
2019-01-27 18:50:55 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. My name is Stef Claus."
+    resp.write "Hello, my name is Stef Claus."
     resp.finish
   end
 
