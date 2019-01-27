cd5f7f867ae9d1cc857b0412ae87dcf89a6f375d
Stef Claus <stef.claus@gmail.com>
2019-01-27 18:50:29 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World. My name is Stef Claus."
     resp.finish
   end
 
 end
-
